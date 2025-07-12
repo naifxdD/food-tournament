526d676579bbc6e806becc6db43e441995489d64
naifxdD <naifaldair@gmail.com>
2025-07-12 23:26:21 +0300

    expo-location integration

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-export default function Index() {
+export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let longitude = "Waiting...";
+  let latitude = "Waiting...";
+  if (errorMsg) {
+    longitude = errorMsg;
+    latitude = errorMsg;
+  } else if (location) {
+    longitude = JSON.stringify(location.coords.longitude);
+    latitude = JSON.stringify(location.coords.latitude);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} provider="google">
+        <Marker
+          coordinate={{
+            latitude: Number(latitude),
+            longitude: Number(longitude),
+          }}
+          title="android"
+        />
+      </MapView>
+      <Text>{longitude}</Text>
+      <Text>{latitude}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
